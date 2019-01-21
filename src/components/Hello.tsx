256a2c30987f38daf7644613d0c06eaddedcae6c
Dilan <dilanozusta@outlook.com>
2019-01-21 15:23:25 -0800

    react app with no state

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
+import  './Hello.css';
 
 export interface Props {
   name: string;
   enthusiasmLevel?: number;
 }
 
-function Hello({name, enthusiasmLevel = 1}: Props) {
+function Hello({ name, enthusiasmLevel = 1 }: Props) {
   if (enthusiasmLevel <= 0) {
     throw new Error('You could be a little more enthusiastic. :D');
   }
