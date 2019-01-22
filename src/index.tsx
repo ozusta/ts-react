fe357a7179ac3784626b43239067e5630c048d25
Dilan <dilanozusta@outlook.com>
2019-01-21 21:49:27 -0800

    used containers intead of components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Hello from './components/Hello';
+import Hello from './containers/Hello';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { Provider } from 'react-redux';
+
+
+const store = createStore(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <Hello name="Dilan" enthusiasmLevel={1} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
